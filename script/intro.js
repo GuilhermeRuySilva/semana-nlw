4fb009217a66b0c7b6b8489885180684b503b8c7
GuilhermeRuySilva <guiruysilva98@gmail.com>
2020-06-03 05:12:11 -0300

    Aula2

---
+++ script/intro.js
@@ -0,0 +1,56 @@
+// // commbnts
+// // document.write("Hello World")
+
+// // var myvar undefined
+
+// let myvar = "Guilherme"
+// const myconst = "Ruy"
+
+// document.write(myvar + myconst)
+
+// string
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 22,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// array - vetores
+
+// const colecao = ["blue", "green", 1, {name: "Guilherme"}]
+
+// document.write(colecao[3].name)
+
+// Funções - Atalhos - Reuso de codigos
+
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// //Executar
+// sayMyName("Guilherme")
+// sayMyName("Samira")
+// sayMyName("Guilherme")
+// sayMyName("Samira")
+
+//conditions
+
+// const notaFinal = 7
+
+// if(notaFinal < 5){
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+//loop repetições
+
+// for (i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
